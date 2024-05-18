134df7bc6fb0e7e4b5a10a16700e26ad99f1b73c
Skulak13 <tomek.skulski@gmail.com>
2024-05-18 18:42:10 +0200

    update

--- script.js
+++ script.js
@@ -15,24 +15,6 @@ let slider = {
         document.getElementById("slider-image").src = this.slides[this.currentSlide];
     },
 
-    autoSlide: setInterval(function () {
-        const autoButton = document.querySelector("#auto-button");
-        if (!autoButton.classList.contains('clicked')) {
-            this.currentSlide++;
-            if (this.currentSlide >= this.slides.length) {
-                this.currentSlide = 0;
-            }
-            document.getElementById("slider-image").src = this.slides[this.currentSlide];
-            autoButton.classList.add('clicked');
-            autoButton.textContent = 'Stop auto';
-        }
-        else {
-            autoButton.classList.remove('clicked');
-            autoButton.textContent = 'Start auto';
-            clearInterval(this.autoSlide);
-        }
-    }.bind(this), 10000),
-
     prevSlide: function () {
         this.currentSlide--;
         if (this.currentSlide < 0) {
@@ -41,10 +23,33 @@ let slider = {
         document.getElementById("slider-image").src = this.slides[this.currentSlide];
     },
 
+    startAutoSlide: function () {
+        this.autoSlideInterval = setInterval(() => {
+            this.nextSlide();
+        }, 10000);
+    },
+
+    stopAutoSlide: function () {
+        clearInterval(this.autoSlideInterval);
+    },
+
+    toggleAutoSlide: function () {
+        const autoButton = document.querySelector("#auto-button");
+        if (!autoButton.classList.contains('clicked')) {
+            this.startAutoSlide();
+            autoButton.classList.add('clicked');
+            autoButton.textContent = 'Stop auto';
+        } else {
+            this.stopAutoSlide();
+            autoButton.classList.remove('clicked');
+            autoButton.textContent = 'Start auto';
+        }
+    },
+
     init: function () {
         document.getElementById("slider-image").src = this.slides[this.currentSlide];
         document.getElementById("prev-button").addEventListener("click", this.prevSlide.bind(this));
-        document.getElementById("auto-button").addEventListener("click", this.nextSlide.bind(this));
+        document.getElementById("auto-button").addEventListener("click", this.toggleAutoSlide.bind(this));
         document.getElementById("next-button").addEventListener("click", this.nextSlide.bind(this));
     }
 };
